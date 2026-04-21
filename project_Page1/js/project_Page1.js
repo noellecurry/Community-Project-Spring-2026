edab50b14dcf895d586ca0abdf6c836e01766efd
noellecurry <924735211@sfsu.edu>
2026-04-20 23:26:26 -0700

    update

--- project_Page1/js/project_Page1.js
+++ project_Page1/js/project_Page1.js
@@ -5,10 +5,11 @@
 
 const container = document.querySelector('.lyricsOne');
 const words = ['sitting'];
+// container and array for the text
 
 let displayInterval = 1000;
 let wordTimer;
-
+// word is added every one second
 
 function removeAllWords() {
     document.querySelectorAll('.falling-word').forEach(word => {
@@ -24,6 +25,7 @@ function startWordCascade() {
         displayRandomWord(words[0]);
     }, displayInterval);
 }
+// starts the repeating loop
 
 function displayRandomWord(text) {
     const wordElem = document.createElement('div');
@@ -35,6 +37,8 @@ function displayRandomWord(text) {
     wordElem.style.top = `${container.querySelectorAll('.falling-word').length * 120}px`;
     container.appendChild(wordElem);
 }
+// adds the css and positions the text
 
 startWordCascade();
+// starts animation again when script runs
 
